b81a2dc922b2585a8a1f0025e9af23825d4dce48
Admin <admin@example.com>
2026-09-25 15:42:53 +0300

    qw

--- README.md
+++ README.md
@@ -32,5 +32,9 @@
 
 ![Задание 5](images/lab01/img05.png)
 
+(images\lab01\img06.png)
+
+
+
 
 
---
+++ src/lab01/ex06.py
@@ -0,0 +1,11 @@
+n = int(input("in_1: "))
+ochno = 0
+zaochno = 0
+for i in range(n):
+    surname, name, age, format = input(f"in_{i + 2}: ").split()
+
+    if format == "True":
+        ochno += 1
+    else:
+        zaochno += 1
+print(f'out : {ochno} {zaochno}')
